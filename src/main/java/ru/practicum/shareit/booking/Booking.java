2f49472f269b3118704be44eff8e95901a586ca8
jokindy <95441202+jokindy@users.noreply.github.com>
2022-07-17 17:00:47 +0300

    test

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -3,8 +3,6 @@ package ru.practicum.shareit.booking;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.practicum.shareit.item.Item;
-import ru.practicum.shareit.user.User;
-
 import java.time.LocalDate;
 
 @Data
@@ -15,6 +13,6 @@ public class Booking {
     private LocalDate start;
     private LocalDate end;
     private Item item;
-    private User booker;
+    private int bookerId;
     private BookingStatus status;
 }
